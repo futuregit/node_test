ef4f1a4170759c441227dc70b307b907195c90b5
futuregit <tomcis102@hotmail.com>
2021-10-29 10:15:10 -0400

    Added some routes

--- app.js
+++ app.js
@@ -5,27 +5,20 @@ const express = require('express');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+
+const userRoutes = require('./routes/user')
+
+app.use(express.urlencoded({ extended: true }));
+
+
 //Middleware
-app.use('/', (req, res, next) => {
-    console.log('This always run')
-    next();
+
+app.use('/admin', adminRoutes);
+app.use(userRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.use('/add-product',(req, res, next) => {
-    console.log("Inside the app.js again");
-    res.send('<form action="/product"><input type="text" name="title"><button type="submit">Add</button></form>')
-});
-
-app.use('/products', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-    console.log("Inside the app.js again");
-    res.send('<h1>Hello everyone</h1>')
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
